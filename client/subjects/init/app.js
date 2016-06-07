a66dae2f0c48159139f1a09852f89f6bdcee4be1
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2016-06-07 16:23:39 +0000

    subjects init

--- client/subjects/init/app.js
+++ client/subjects/init/app.js
@@ -1,2 +1,17 @@
 // define app module
-// define a run function
+var app =angular
+  .module("artooInit", [])
+  .run(() => {
+    console.info("I run in the setter");
+  });
+
+// get app module and execute .run() method
+angular
+  .module("artooInit")
+  .run(() => {
+    console.info("I run in the getter");
+  });
+
+app.run(() => {
+  console.info("I run from the module in the var");
+});
