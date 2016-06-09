4306849be62d53ba05a8bfc155939447b565f55a
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2016-06-09 10:54:22 +0000

    subject forms

--- client/subjects/forms/items.controller.js
+++ client/subjects/forms/items.controller.js
@@ -1,3 +1,31 @@
-angular.module('artoo').controller('ItemsCtrl', function ($scope) {
-  $scope.items = [];
+angular.module('artoo').controller('ItemsCtrl', function ($scope, ItemsSrv) {
+  
+  $scope.ItemsSrv = ItemsSrv;
+  var masterItem;
+  
+  // Setting default form values for the selected item
+  $scope.selectItem = function (code) {
+    var masterItem = $scope.item = angular.copy(ItemsSrv.find(code));
+  };
+  
+  // Initializing form fields (not needed: form appears when an item is selected)
+  $scope.selectedItem = ItemsSrv.get()[0].code;
+  $scope.selectItem($scope.selectedItem);
+  
+  $scope.insert = function (item, form) {
+    ItemsSrv.insert(item);
+    $scope.resetForm(form);
+  };
+  
+  $scope.resetForm = function (form) {
+    $scope.item = angular.copy(masterItem);
+    //$scope.item = {};
+    //form.$setPristine();
+  };
+  
+  $scope.update = function (item, form) {
+    ItemsSrv.update(item);
+    masterItem = angular.copy(item);
+  };
+  
 });
