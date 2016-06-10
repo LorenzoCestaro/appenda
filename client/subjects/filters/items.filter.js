05e3724f1323c6310e1ed86a350e8da622f9fe0a
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2016-06-10 09:37:56 +0000

    subject filters

---
+++ client/subjects/filters/items.filter.js
@@ -0,0 +1,39 @@
+// filters names are in camelCase by convention
+// filters can be chained after the module getter
+angular.module('artoo')
+
+  .filter('price', function () {
+    return function (input, currency = true) {
+      var output = input.toFixed(2);
+      return (currency) ? output + ' gold' : output;
+    };
+  })
+  
+  .filter('availability', function () {
+    return function (input) {
+      return (parseInt(input)) ? input + ' pieces' : 'out of stock';
+    };
+  })
+  
+  .filter('races', function () {
+    return function (input) {
+      return input.join(' - ') + '.';
+    };
+  })
+  
+  .filter('suitable', function (PlayerSrv) {
+    var race = PlayerSrv.get().race;
+    
+    return function (input) {
+      return input.indexOf(race) > -1;
+    };
+  })
+  
+  .filter('suitability', function (PlayerSrv) {
+    var race = PlayerSrv.get().race;
+    
+    return function (input, active) {
+      return (active && input.filter(singleItem => singleItem.races.indexOf(race) > -1)) || input;
+    };
+  });
+  
---
+++ client/subjects/filters/player.service.js
@@ -0,0 +1,11 @@
+angular.module('artoo').service('PlayerSrv', function () {
+  
+  var player = {
+    name: 'Legolas',
+    race: 'elf',
+  };
+  
+  this.get = function () {
+    return player;
+  }; 
+});
