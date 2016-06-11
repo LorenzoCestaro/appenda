75fcec7cf94e1b6b359aac3d8f51e9e65ec04e03
David Sorrentino <me@davidsorrentino.com>
2016-06-11 08:59:34 +0200

    Update contacts.service.js

--- client/project/contact/contacts.service.js
+++ client/project/contact/contacts.service.js
@@ -2,16 +2,14 @@ angular.module('appenda').service('ContactsSrv', function () {
   
   var maxId = 1;
   
-  var contacts = [
-      {
-        id: 0,
-        name: 'Silvia',
-        surname: 'Peveri',
-        phone: '3333333333',
-        email: 'silvia.peveri@gmail.com',
-        img: 'http://placehold.it/20x20',
-      },
-    ];
+  var contacts = [{
+    id: 0,
+    name: 'Silvia',
+    surname: 'Peveri',
+    phone: '3333333333',
+    email: 'silvia.peveri@gmail.com',
+    img: 'http://placehold.it/20x20',
+  },];
   
   this.create = function (formInput) {
     formInput.id = maxId;
