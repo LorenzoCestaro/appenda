a070cd50d22c27fcc52e02708b2a24a57d2858f8
Lorenzo Cestaro <cestaro.lorenzo@gmail.com>
2016-06-20 14:09:10 +0000

    project feature: automatic capitalization in contact names and event titles

--- client/project/add-contact/add-contact.controller.js
+++ client/project/add-contact/add-contact.controller.js
@@ -3,6 +3,8 @@ angular.module('appenda').controller('AddContactCtrl', function ($scope, $state,
   $scope.newContact = ContactsSrv.create();
   
   $scope.save = function (newContact, form) {
+    newContact.name = newContact.name.charAt(0).toUpperCase() + newContact.name.slice(1);
+    newContact.surname = newContact.surname.charAt(0).toUpperCase() + newContact.surname.slice(1);
     newContact.$save()
       .then((data) => {
         newContact = ContactsSrv.create();
